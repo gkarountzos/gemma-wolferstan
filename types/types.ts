ea0a55efd1f3e6226fe8a1ea90e0bc5c4082754c
George Karountzos <george3-2001@hotmail.com>
2024-08-23 00:37:13 +0300

    ensured type safety

---
+++ types/types.ts
@@ -0,0 +1,33 @@
+export interface Project {
+  title: string;
+  description: string;
+  link: string;
+  image: string;
+}
+
+export interface Section {
+  name: string;
+  href: string;
+}
+
+export interface SocialLink {
+  href: string;
+  icon: React.ComponentType<{ size: number; className?: string }>;
+}
+
+export interface BackgroundGradientAnimationProps {
+  gradientBackgroundStart?: string;
+  gradientBackgroundEnd?: string;
+  firstColor?: string;
+  secondColor?: string;
+  thirdColor?: string;
+  fourthColor?: string;
+  fifthColor?: string;
+  pointerColor?: string;
+  size?: string;
+  blendingValue?: string;
+  children?: React.ReactNode;
+  className?: string;
+  interactive?: boolean;
+  containerClassName?: string;
+}
