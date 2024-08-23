009305ef96965b40d48c6c70d64de2cf0646ed91
Gemma-Wolferstan <g.wolferstan@gmail.com>
2024-08-23 17:40:14 +0100

    Changed cringe text

--- components/About.tsx
+++ components/About.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 
-const aboutText: string[] = ["Hungry cats"];
+const aboutText: string[] = [
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sit amet tellus efficitur, sodales tortor vitae, suscipit nisl. Nunc sapien lorem, semper nec maximus eu, semper at ipsum. Etiam vitae sagittis magna. Sed vitae cursus massa. Vivamus nibh sem, pulvinar aliquet urna vitae, faucibus volutpat enim. Phasellus nec efficitur odio. Phasellus et ullamcorper neque, eget mattis enim. Integer at lorem suscipit, ornare risus efficitur, congue dolor. Etiam accumsan tellus at tortor sagittis, eget facilisis ipsum egestas. Sed egestas a sapien et hendrerit. Sed nec pulvinar est. ",
+];
 
 const About: React.FC = () => {
   return (
