cdbbe363667323651844664437b2207a037eed86
George Karountzos <george3-2001@hotmail.com>
2024-08-23 18:23:36 +0300

    added tracing beam

--- components/Content.tsx
+++ components/Content.tsx
@@ -2,14 +2,17 @@ import About from "./About";
 import Experience from "./Experience";
 import Footer from "./Footer";
 import Projects from "./Projects";
+import { TracingBeam } from "./ui/tracing-beam";
 
 export default function Content() {
   return (
-    <div className="space-y-28">
-      <About />
-      <Experience />
-      <Projects />
-      <Footer />
-    </div>
+    <TracingBeam className="flex gap-4">
+      <div className="space-y-28">
+        <About />
+        <Experience />
+        <Projects />
+        <Footer />
+      </div>
+    </TracingBeam>
   );
 }
