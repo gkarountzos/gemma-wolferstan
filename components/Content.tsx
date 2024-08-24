d7165d3fe0a7dc1f96a4b92feb9ed1d171dc4dfe
George Karountzos <george3-2001@hotmail.com>
2024-08-24 18:02:30 +0300

    hotfix

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,14 +1,9 @@
 import MainContent from "./MainContent";
 import Sidebar from "./Sidebar";
-import { DotBackground } from "./ui/DotBackground";
 
 const Content = () => {
-  const background = `fixed inset-0 -z-10`;
   return (
     <>
-      <div className={background}>
-        <DotBackground />
-      </div>
       <div className="flex justify-center gap-4 ">
         <div className="w-1/2">
           <Sidebar />
