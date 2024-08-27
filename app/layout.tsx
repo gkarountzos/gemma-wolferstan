5fa2e6d0f8b31217d3805e1aa23ed06db147b709
George Karountzos <george3-2001@hotmail.com>
2024-08-27 14:19:07 +0300

    minor tweaks

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className}`}>
         <DotBackground>
-          <div className="flex flex-col lg:flex-row min-h-screen px-4 xs:px-6 sm:px-10 md:px-24 xxl:px-60 xxxl:px-72 fourk:px-[550px]">
+          <div className="flex flex-col lg:flex-row min-h-screen px-4 xs:px-6 sm:px-10 md:px-24 xxl:px-52 xxxl:px-72 fourk:px-[550px]">
             <div className="w-full lg:w-1/2">
               <Sidebar />
             </div>
