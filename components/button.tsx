8d96ee3105524c4cdf6ad8d30bb9fa23e2513266
George Karountzos <george3-2001@hotmail.com>
2024-10-07 22:25:08 +0300

    final design changes

--- components/button.tsx
+++ components/button.tsx
@@ -1,13 +1,21 @@
 import Link from "next/link";
 
-export default function Button({ children }: { children: React.ReactNode }) {
+export default function Button({
+  children,
+  className,
+  link,
+}: {
+  children: React.ReactNode;
+  className?: string;
+  link: string;
+}) {
   return (
     <div className="mt-6">
       <Link
-        href="/resume.pdf"
+        href={`${link}`}
         target="_blank"
         rel="noopener noreferrer"
-        className="flex justify-center items-center just w-fit mx-auto px-4 py-2 rounded-full bg-[#4e2780] text-white transition duration-200"
+        className={`${className} flex justify-center items-center w-fit mx-auto px-4 py-2 rounded-full bg-[#4e2780] text-white transition duration-200 shadow-md md:hover:shadow-[0_0_15px_#ffffff]`}
       >
         {children}
       </Link>
